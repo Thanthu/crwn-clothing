2fee44873d0d8d62b71e6d5418eb35df61a2a8d2
Thanthu <tnair@orthofx.com>
2022-04-22 16:06:01 +0530

    create SignIn route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import NavBar from "./routes/navbar/NavBar";
 import Home from "./routes/home/Home";
+import SignIn from "./routes/sign-in/SignIn";
 
 function Shop() {
   return <h1>I am the Shop page</h1>
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<NavBar />} >
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
