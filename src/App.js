4cc689a700f3324df0fc7011eb407def3c42a9eb
Thanthu <tnair@orthofx.com>
2022-04-22 13:00:07 +0530

    create dummy NavBar and Shop page components and setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/Home";
+
+function NavBar() {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+function Shop() {
+  return <h1>I am the Shop page</h1>
+}
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<NavBar />} >
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
